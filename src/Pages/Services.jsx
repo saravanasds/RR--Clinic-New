9c369445d4a369d6abed0b30840cd8f2fd2b4a86
naveen1798kumar <naveennaveennk1798@gmail.com>
2024-07-10 16:10:59 +0530

    updated underprocess

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 const Services = () => {
   return (
    <>
-      <div>
-        
+      <div className="flex justify-center items-center h-screen bg-gray-100" data-aos="fade-left">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-blue-600 mb-4">
+          Under Progress
+        </h1>
+        <p className="text-xl text-gray-700">
+          We are working hard to bring you this page. Stay tuned!
+        </p>
       </div>
+    </div>
    </>
   )
 }
